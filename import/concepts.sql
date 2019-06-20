e7d3b816c4492f78bd107cbe0a2eda6787cc98c8
Clark Evans <cce@clarkevans.com>
2019-06-20 11:58:11 -0400

    concept_ancestor flattens the hierarchy

--- import/concepts.sql
+++ import/concepts.sql
@@ -4,6 +4,12 @@ CREATE TEMPORARY TABLE concepts (id INTEGER);
 INSERT INTO concepts(id) values (44819025), (44819232), (7), (44819279), (44819147), (44819233), (44819247), (44819097), (44818997), (44818821), (19), (44818723), (44819235), (44819274);
 --INSERT INTO concepts(id) values (316866); -- Hypertensive Disorder
 
+INSERT INTO concepts(id)
+ SELECT ancestor_concept_id as id
+   FROM concept_ancestor
+   JOIN concepts ON (concepts.id = concept_ancestor.descendant_concept_id)
+ EXCEPT SELECT id from concepts;
+
 -- calculate ancestor concepts
 WITH RECURSIVE t(n) AS (
     SELECT id FROM concepts
@@ -13,12 +19,5 @@ WITH RECURSIVE t(n) AS (
 INSERT INTO concepts(id)
  SELECT DISTINCT n FROM t
   EXCEPT SELECT id from concepts;
-WITH RECURSIVE t(n) AS (
-    SELECT id FROM concepts
-  UNION ALL
-    SELECT ancestor_concept_id as id FROM concept_ancestor, t WHERE t.n = concept_ancestor.descendant_concept_id
-)
-INSERT INTO concepts(id)
- SELECT DISTINCT n FROM t
-  EXCEPT SELECT id from concepts;
+
 \copy (SELECT * FROM concepts) TO 'concepts.csv' CSV HEADER;
