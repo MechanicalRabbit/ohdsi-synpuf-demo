dfcaa117413d7492462c294c633ffa4885f5c025
Clark Evans <cce@clarkevans.com>
2019-06-20 12:06:37 -0400

    add Hypertensive Disorder

--- import/concepts.sql
+++ import/concepts.sql
@@ -1,8 +1,8 @@
 -----------------------------------
 -- generate list of concepts included
 CREATE TEMPORARY TABLE concepts (id INTEGER);
-INSERT INTO concepts(id) values (44819025), (44819232), (7), (44819279), (44819147), (44819233), (44819247), (44819097), (44818997), (44818821), (19), (44818723), (44819235), (44819274);
---INSERT INTO concepts(id) values (316866); -- Hypertensive Disorder
+INSERT INTO concepts(id) values (44819025), (44819232), (7), (44819279), (44819147), (44819233), (44819247), (44819097), (44818997), (44818821), (19), (44818723), (44819235), (44819274), (44819058);
+INSERT INTO concepts(id) values (316866); -- Hypertensive Disorder
 
 INSERT INTO concepts(id)
  SELECT ancestor_concept_id as id
--- import/export.sql
+++ import/export.sql
@@ -8,6 +8,6 @@ CREATE TEMPORARY TABLE concepts (id INTEGER);
 \copy (SELECT * FROM concept WHERE concept_id in (SELECT id FROM concepts) ORDER BY concept_id) TO 'concept.csv' WITH CSV HEADER
 \copy (SELECT * FROM vocabulary WHERE vocabulary_id in ('Concept Class','Vocabulary','Domain','SNOMED','Relationship') ORDER BY vocabulary_id) TO 'vocabulary.csv' WITH CSV HEADER
 \copy (SELECT * FROM domain WHERE domain_id in ('Metadata','Condition') ORDER BY domain_id) TO 'domain.csv' WITH CSV HEADER
-\copy (SELECT * FROM concept_class WHERE concept_class_id in ('Vocabulary','Concept Class','Domain','Clinical Finding','Relationship') ORDER BY concept_class_id) TO 'concept_class.csv' WITH CSV HEADER
+\copy (SELECT * FROM concept_class WHERE concept_class_id in ('Vocabulary','Concept Class','Domain','Clinical Finding','Relationship','Model Comp') ORDER BY concept_class_id) TO 'concept_class.csv' WITH CSV HEADER
 \copy (SELECT * FROM concept_ancestor WHERE descendant_concept_id in (SELECT id FROM concepts) ORDER BY descendant_concept_id) TO 'concept_ancestor.csv' WITH CSV HEADER
 \copy (SELECT * FROM concept_relationship WHERE concept_id_1 in (SELECT id FROM concepts) AND relationship_id IN ('Is a') ORDER BY concept_id_1) TO 'concept_relationship.csv' WITH CSV HEADER
