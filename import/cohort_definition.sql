d6adab3e6eaf3accdbd56b1a81e543ad7ccf2597
Clark Evans <cce@clarkevans.com>
2019-06-22 11:01:21 -0400

    recording synpuf5 overlaps among cohorts

--- import/cohort_definition.sql
+++ import/cohort_definition.sql
@@ -4,3 +4,13 @@ VALUES
 (1770674,44819246,56,'Acute myocardial infarction events'),
 (1770675,44819246,56,'New users of ACE inhibitors as first-line monotherapy for hypertension'),
 (1770676,44819246,56,'New users of Thiazide-like diuretics as first-line monotherapy for hypertension');
+
+CREATE OR REPLACE VIEW cohort_ang AS (select * from cohort where
+    cohort_definition_id = 1770673);
+CREATE OR REPLACE VIEW cohort_ami AS (select * from cohort where
+    cohort_definition_id = 1770674);
+CREATE OR REPLACE VIEW cohort_ace AS (select * from cohort where
+    cohort_definition_id = 1770675);
+CREATE OR REPLACE VIEW cohort_dia AS (select * from cohort where
+    cohort_definition_id = 1770676);
+
