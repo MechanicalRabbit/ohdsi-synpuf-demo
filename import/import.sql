6fceea45039e3f21012359a4576a91b868c6ed31
Clark Evans <cce@clarkevans.com>
2019-06-22 15:23:45 -0400

    observation period

--- import/import.sql
+++ import/import.sql
@@ -7,5 +7,6 @@
 \copy concept FROM 'concept.csv' WITH CSV HEADER
 \copy location FROM 'location.csv' WITH CSV HEADER
 \copy person FROM 'person.csv' WITH CSV HEADER
+\copy observation_period FROM 'observation_period.csv' WITH CSV HEADER
 -- \copy visit_occurrence FROM 'visit_occurrence.csv' WITH CSV HEADER
 \include cohort_definition.sql
