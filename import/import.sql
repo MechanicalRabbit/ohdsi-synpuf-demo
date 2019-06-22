bf6668ce913fa95f0765ca5dc0300571e84e7c6e
Clark Evans <cce@clarkevans.com>
2019-06-22 16:16:18 -0400

    adding provider and visits

--- import/import.sql
+++ import/import.sql
@@ -9,5 +9,6 @@
 \copy person FROM 'person.csv' WITH CSV HEADER
 \copy observation_period FROM 'observation_period.csv' WITH CSV HEADER
 \copy care_site FROM 'care_site.csv' WITH CSV HEADER
--- \copy visit_occurrence FROM 'visit_occurrence.csv' WITH CSV HEADER
+\copy provider FROM 'provider.csv' WITH CSV HEADER
+\copy visit_occurrence FROM 'visit_occurrence.csv' WITH CSV HEADER
 \include cohort_definition.sql
