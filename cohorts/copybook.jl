ee702e1c34118bc7617e90eb0ef0a5b93511dd0a
Clark Evans <cce@clarkevans.com>
2019-06-26 21:55:29 -0400

    very simple combinator to test for hypertension

--- cohorts/copybook.jl
+++ cohorts/copybook.jl
@@ -2,10 +2,6 @@ using LibPQ, DataKnots, DataKnots4Postgres
 using DataKnots: Query, Environment, Pipeline, target, lookup, 
                  compose, cover, syntaxof
 
-
-#
-#
-
 CascadeGet(names...) =
     Query(CascadeGet, names...)
  
@@ -26,7 +22,15 @@ const Concept = CascadeGet(:concept, :fpk_observation_concept,
                            :fpk_device_concept, :fpk_procedure_concept,
                            :fpk_drug_era_concept, :fpk_drug_concept) >>
                 Label(:concept)
-
 DataKnots.translate(::Module, ::Val{:concept}) = Concept
 
+AnyOf(Xs...) = Lift(|, (Xs...,))
+OneOf(X, Ys...) = AnyOf((X .== Y for Y in Ys)...)
+HasCode(vocabulary_id, codes...) = 
+    Concept >> 
+    Filter((It.vocabulary_id .== string(vocabulary_id)) .&
+           OneOf(It.concept_code, string.(codes)...)) >>
+    Exists
+
 sp10 = DataKnot(LibPQ.Connection(""))
+
