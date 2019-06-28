cbfe1636088a69f6a0009333c17029231159b6fc
Clark Evans <cce@clarkevans.com>
2019-06-28 10:03:45 -0400

    fixing start/end combinators

--- cohorts/copybook.jl
+++ cohorts/copybook.jl
@@ -39,20 +39,21 @@ const StartDate =
                    :visit_start_date, :observation_period_start_date,
                    :drug_exposure_start_date, :drug_era_start_date,
                    :procedure_date, :device_exposure_start_date) >>
+        Date.(It) >>
         Label(:start_date))
 
 translate(::Module, ::Val{:start_date}) = StartDate
 
 const EndDate =
     Is1to1(
-        Given(:start_date => StartDate,
+           Given(:start_date => StartDate,
               :end_date => CascadeGet(:end_date, :condition_end_date,
                                       :visit_end_date, :procedure_date,
                                       :observation_period_end_date,
                                       :device_exposure_end_date,
                                       :drug_exposure_end_date,
                                       :drug_era_end_date),
-              coalesce.(It.end_date, It.start_date)) >>
+              coalesce.(It.end_date, It.start_date .+ Day(1))) >>
         Date.(It) >>
         Label(:end_date))
 
