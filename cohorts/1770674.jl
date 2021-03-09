1d15cb2812ef6fc78c49a6b193f8579b94cf1340
Kirill Simonov <xi@resolvent.net>
2021-03-09 13:17:00 -0600

    improving the cohort query

--- cohorts/1770674.jl
+++ cohorts/1770674.jl
@@ -14,9 +14,9 @@ conn = LibPQ.Connection("postgresql:///synpuf-5pct?host=/var/run/postgresql")
 
 function run(q)
     sql = to_sql(normalize(q))
-    println(sql, ";")
-    println()
-    @time result = execute(conn, sql)
+    #println(sql, ";")
+    #println()
+    result = execute(conn, sql)
     println(convert(DataKnot, result))
     println('-'^80)
 end
@@ -46,15 +46,15 @@ const old_myocardial_infarction = SNOMED("1755008")
 const inpatient_or_er = VISIT("ERIP", "ER", "IP")
 
 IsCoded(t) =
-    Fun.And(Fun."="(Get.vocabulary_id, t.codesystem),
-            Fun."="(Get.concept_code, t.code))
+    Fun.And(Get.vocabulary_id .== t.codesystem,
+            Get.concept_code .== t.code)
 
 IsCoded(ts::AbstractVector) =
     Fun.Or((IsCoded(t) for t in ts)...)
 
 FromConcept() =
     From(concept) |>
-    Where(Fun."IS_NULL"(Get.invalid_reason))
+    Where(ismissing.(Get.invalid_reason))
 
 FromConceptOnly(t) =
     FromConcept() |> Where(IsCoded(t))
@@ -64,17 +64,17 @@ FromConcept(t) =
     Append(
         FromConcept() |>
         Join(concept_ancestor,
-             Fun."="(Get.concept_id, Get.descendant_concept_id)) |>
+             Get.concept_id .== Get.descendant_concept_id) |>
         Join(:ancestor => FromConceptOnly(t),
-             Fun."="(Get.ancestor_concept_id, Get.ancestor.concept_id))) |>
+             Get.ancestor_concept_id .== Get.ancestor.concept_id)) |>
     ((t isa AbstractVector) ? Distinct(Get.concept_id) : identity)
 
 FromConcept(t, et) =
     FromConcept(t) |>
     Join(:excluded => FromConcept(et),
-         Fun."="(Get.concept_id, Get.excluded.concept_id),
+         Get.concept_id .== Get.excluded.concept_id,
          is_left=true) |>
-    Where(Fun."IS NULL"(Get.excluded.concept_id))
+    Where(ismissing.(Get.excluded.concept_id))
 
 QInfarctions =
     FromConcept(myocardial_infarction,
@@ -101,42 +101,42 @@ run(QInpatientOrER)
 QInfarctionConditions =
     From(condition_occurrence) |>
     Join(:infarction_concept => QInfarctions,
-         Fun."="(Get.condition_concept_id, Get.infarction_concept.concept_id)) |>
+         Get.condition_concept_id .== Get.infarction_concept.concept_id) |>
     Define(:index_date => Get.condition_start_date)
 run(QInfarctionConditions |> Select(Get.person_id, Get.index_date))
 
 QInfarctionConditionsInOP =
     QInfarctionConditions |>
     Join(:OP => observation_period,
-         Fun.And(Fun."="(Get.person_id, Get.OP.person_id),
-                 Fun."<="(Get.OP.observation_period_start_date, Get.index_date),
-                 Fun."<="(Get.index_date, Get.OP.observation_period_end_date)))
+         Fun.And(Get.person_id .== Get.OP.person_id,
+                 Get.OP.observation_period_start_date .<= Get.index_date,
+                 Get.index_date .<= Get.OP.observation_period_end_date))
 run(QInfarctionConditionsInOP |>
     Select(Get.person_id,
            Get.OP.observation_period_start_date,
            :start_date => Get.index_date,
-           :end_date => Fun."+"(Get.index_date, 7),
+           :end_date => Get.index_date .+ 7,
            Get.OP.observation_period_end_date))
 
 QAcuteVisits =
     From(visit_occurrence) |>
     Join(:inpatient_or_er_concept => QInpatientOrER,
-         Fun."="(Get.visit_concept_id, Get.inpatient_or_er_concept.concept_id))
+         Get.visit_concept_id .== Get.inpatient_or_er_concept.concept_id)
 run(QAcuteVisits)
 
 QInfarctionConditionsInOPDuringAcuteVisit =
     QInfarctionConditionsInOP |>
     Join(:acute_visit => QAcuteVisits,
-         Fun.And(Fun."="(Get.person_id,
-                         Get.acute_visit.person_id),
-                 Fun."<="(Get.OP.observation_period_start_date,
-                          Get.acute_visit.visit_start_date),
-                 Fun."<="(Get.acute_visit.visit_end_date,
-                          Get.OP.observation_period_end_date),
-                 Fun."<="(Get.acute_visit.visit_start_date,
-                          Get.index_date),
-                 Fun."<="(Get.index_date,
-                          Get.acute_visit.visit_end_date)))
+         Fun.And(Get.person_id
+                    .== Get.acute_visit.person_id,
+                 Get.OP.observation_period_start_date
+                    .<= Get.acute_visit.visit_start_date,
+                 Get.acute_visit.visit_end_date
+                    .<= Get.OP.observation_period_end_date,
+                 Get.acute_visit.visit_start_date
+                    .<= Get.index_date,
+                 Get.index_date
+                    .<= Get.acute_visit.visit_end_date))
 run(QInfarctionConditionsInOPDuringAcuteVisit)
 
 QCorrelatedAcuteVisit(person_id, index_date, op_start_date, op_end_date) =
@@ -168,7 +168,7 @@ run(QInfarctionConditionsInOPDuringAcuteVisit)
 
 run(QInfarctionConditionsInOPDuringAcuteVisit |>
     Group(Get.person_id) |>
-    Where(Fun.">"(Agg.Count(Get.index_date, distinct=true), 3)))
+    Where(Agg.Count(Get.index_date, distinct=true) .> 3))
 
 run(person |>
     Window(Get.gender_concept_id) |>
@@ -176,21 +176,38 @@ run(person |>
 
 const selected_person_id = 42891
 
+CollapseIndexDate(days) =
+    Define(:end_date => Get.index_date .+ days) |>
+    Window(Get.person_id, order=[Get.index_date]) |>
+    Define(:boundary => Agg.Lag(Get.end_date)) |>
+    Define(:bump => Fun.Case(Get.index_date .<= Get.boundary, 0, 1)) |>
+    Window(Get.person_id, order=[Get.index_date]) |>
+    Define(:group => Agg.Sum(Get.bump)) |>
+    Group(Get.person_id, Get.group) |>
+    Define(:min_date => Agg.Min(Get.index_date),
+           :max_date => Agg.Max(Get.index_date))
+
+q = QInfarctionConditionsInOPDuringAcuteVisit |>
+    Where(Get.person_id .== selected_person_id) |>
+    CollapseIndexDate(180) |>
+    Select(Get.person_id, Get.min_date, Get.max_date);
+run(q)
+
 QInfarctionConditionsInOPDuringAcuteVisitCollapsedExplanation =
     QInfarctionConditionsInOPDuringAcuteVisit |>
-    Where(Fun."="(Get.person_id, selected_person_id)) |>
+    Where(Get.person_id .== selected_person_id) |>
     Window(Get.person_id, order=[Get.index_date]) |>
-    Define(:boundary => Fun."+"(Agg.Lag(Get.index_date), 180)) |>
-    Define(:bump => Fun.Case(Fun."<="(Get.index_date, Get.boundary), 0, 1)) |>
+    Define(:boundary => Agg.Lag(Get.index_date) .+ 180) |>
+    Define(:bump => Fun.Case(Get.index_date .<= Get.boundary, 0, 1)) |>
     Select(Get.person_id, Get.index_date, Get.boundary, Get.bump)
 run(QInfarctionConditionsInOPDuringAcuteVisitCollapsedExplanation)
 
 QInfarctionConditionsInOPDuringAcuteVisitCollapsedExplanation2 =
     QInfarctionConditionsInOPDuringAcuteVisit |>
-    Where(Fun."="(Get.person_id, selected_person_id)) |>
+    Where(Get.person_id .== selected_person_id) |>
     Window(Get.person_id, order=[Get.index_date]) |>
-    Define(:boundary => Fun."+"(Agg.Lag(Get.index_date), 180)) |>
-    Define(:bump => Fun.Case(Fun."<="(Get.index_date, Get.boundary), 0, 1)) |>
+    Define(:boundary => Agg.Lag(Get.index_date) .+ 180) |>
+    Define(:bump => Fun.Case(Get.index_date .<= Get.boundary, 0, 1)) |>
     Window(Get.person_id, order=[Get.index_date]) |>
     Define(:group => Agg.Sum(Get.bump)) |>
     Select(Get.person_id, Get.index_date, Get.boundary, Get.bump, Get.group)
@@ -199,8 +216,8 @@ run(QInfarctionConditionsInOPDuringAcuteVisitCollapsedExplanation2)
 QInfarctionConditionsInOPDuringAcuteVisitCollapsed =
     QInfarctionConditionsInOPDuringAcuteVisit |>
     Window(Get.person_id, order=[Get.index_date]) |>
-    Define(:boundary => Fun."+"(Agg.Lag(Get.index_date), 180)) |>
-    Define(:bump => Fun.Case(Fun."<="(Get.index_date, Get.boundary), 0, 1)) |>
+    Define(:boundary => Agg.Lag(Get.index_date) .+ 180) |>
+    Define(:bump => Fun.Case(Get.index_date .<= Get.boundary, 0, 1)) |>
     Window(Get.person_id, order=[Get.index_date]) |>
     Define(:group => Agg.Sum(Get.bump)) |>
     Group(Get.person_id, Get.group) |>
